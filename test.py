27806c3976dcb2190002a29537b1c1331c3568da
1mkRE <macela1402@gmail.com>
2023-01-08 16:08:24 +0100

    Design change to customtkinter.

--- test.py
+++ test.py
@@ -4,8 +4,9 @@ api_key = ''
 
 def chatAI(request):
     response = openai.Completion.create(model="text-davinci-003", prompt=request, max_tokens=1024, api_key=api_key)
+    print(response)
     answer = response.choices[0].text
     return answer
 
-result = chatAI('Hauptstadt von Bosnien')
+result = chatAI('Berechne mir die quadratwurzel von 3')
 print(result)
